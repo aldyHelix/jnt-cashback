0c6d62d781610d3c130767808c93509c42972df6
Aldi Satria Gumilar <aldi.gumilar@tapera.go.id>
2023-07-16 22:08:54 +0700

    summary dpf

--- app/Services/Pivot.php
+++ app/Services/Pivot.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class Pivot {
+    /**
+     * CP DP
+     */
     public function getPivotAllCountSumCPDP($schema){
         if(!Schema::hasTable($schema.'.data_mart')) {
             return false;
@@ -62,6 +65,67 @@ class Pivot {
             return false;
         }
         return DB::table($schema.'.cp_dp_mp_retur_sum_biaya_kirim')
+            ->get();
+    }
+    /**
+     * DPF
+     */
+    public function getPivotDPFAllCountSum($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_all_count_sum')
+            ->get();
+    }
+
+    public function getPivotDPFRegulerCountSum($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_reguler_count_sum')
+            ->get();
+    }
+
+    public function getPivotDPFSuperCountSum($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_super_count_sum')
+            ->get();
+    }
+    public function getPivotDPFDfodCountSum($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_dfod_count_sum')
+            ->get();
+    }
+    public function getPivotDPFMPCountWaybill($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_mp_count_waybill')
+            ->get();
+    }
+    public function getPivotDPFMPSumBiayaKirim($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_mp_sum_biaya_kirim')
+            ->get();
+    }
+    public function getPivotDPFMPReturCountWaybill($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_mp_retur_count_waybill')
+            ->get();
+    }
+    public function getPivotDPFMPReturSumBiayaKirim($schema){
+        if(!Schema::hasTable($schema.'.data_mart')) {
+            return false;
+        }
+        return DB::table($schema.'.dpf_mp_retur_sum_biaya_kirim')
             ->get();
     }
 }
