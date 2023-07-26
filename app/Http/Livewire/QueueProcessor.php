7ba74b9f8a418d6fba42ac8fbf7c919315412fa7
Aldi Satria Gumilar <aldi.gumilar@tapera.go.id>
2023-07-26 14:08:22 +0700

    queue processor

--- app/Http/Livewire/QueueProcessor.php
+++ app/Http/Livewire/QueueProcessor.php
@@ -1,25 +1,29 @@
 <?php
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Queue;
 use Livewire\Component;
 use Illuminate\Support\Facades\Redis;
 
 class QueueProcessor extends Component
 {
+    public $queueName = 'default';
     public $currentJob;
     public $isLoading = false;
 
-    public function processQueue()
+    public function mount()
     {
-        $this->isLoading = true;
+        $this->updateQueueStatus();
 
-        // Start the custom queue worker command
-        Artisan::call('custom:queue-worker', [
-            'connection' => config('queue.default'),
-            '--queue' => 'default',
-        ]);
+        // Refresh queue status every 5 seconds (adjust the interval as needed)
+        $this->dispatchBrowserEvent('start-queue-status-updates', ['interval' => 5000]);
+    }
 
-        $this->isLoading = false;
+    public function updateQueueStatus()
+    {
+        $connection = config('queue.default');
+        $queue = Queue::connection($connection)->size($this->queueName);
+        $this->isLoading = $queue > 0;
     }
 
     public function render()
