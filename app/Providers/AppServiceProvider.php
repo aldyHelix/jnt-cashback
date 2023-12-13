63893a290791e29a90bd566e78ece832e6c19119
Aldy Satria Gumilar <aldy.satria07@gmail.com>
2023-12-13 12:06:07 +0700

    update provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,18 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->singleton(\App\Services\CreateSchemaService::class);
+        $this->app->singleton(\App\Services\FileProcessingService::class);
+        $this->app->singleton(\App\Services\FlagUpdaterService::class);
+        $this->app->singleton(\App\Services\GenerateDPFService::class);
+        $this->app->singleton(\App\Services\GenerateExportService::class);
+        $this->app->singleton(\App\Services\GeneratePivotRekapService::class);
+        $this->app->singleton(\App\Services\GeneratePivotTableService::class);
+        $this->app->singleton(\App\Services\GeneratePivotZonasiService::class);
+        $this->app->singleton(\App\Services\GenerateSchemaService::class);
+        $this->app->singleton(\App\Services\GenerateSummaryService::class);
+        $this->app->singleton(\App\Services\GradingService::class);
+        $this->app->singleton(\App\Services\PivotService::class);
     }
 
     /**
